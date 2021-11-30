329ee3e2b103618f3c43b6724f08fdc4bc6952da
Lijuet <sleechae@gmail.com>
2021-11-30 20:49:05 +0900

    [1130] telegram message

--- crawl-quasarzone.py
+++ crawl-quasarzone.py
@@ -1,7 +1,11 @@
 import requests
-from bs4 import BeautifulSoup as bs
 from datetime import date, timedelta
 import re
+import time
+from bs4 import BeautifulSoup as bs
+import telegram
+
+bot = telegram.Bot(token=key)
 
 target = {
     'cpu':{
@@ -10,7 +14,7 @@ target = {
     },
     'gpu':{
         'name':'3060 ti',
-        'maximum_cost': 700000,
+        'maximum_cost': 7000000,
     },
 }
 
@@ -38,45 +42,73 @@ def reformat_date(item):
 def search_target(type):
     formatted_target = target[type]['name'].replace(" ", "+")
     target_maximum_cost = target[type]['maximum_cost']
-    # search_url = "https://quasarzone.com/bbs/qb_saleinfo?_method=post&type=&page=1&_token=Q7kaCjkdlWWa9Q6lPxPZopa4URVoxDkq4VKwgCFM&category=&popularity=&kind=subject&keyword="+formatted_target+"&sort=num%2C+reply&direction=DESC"
-    # general_url = "https://quasarzone.com/bbs/qb_saleinfo?_method=post&type=&page=1&_token=Q7kaCjkdlWWa9Q6lPxPZopa4URVoxDkq4VKwgCFM&category=&popularity=&kind=subject&keyword=&sort=num%2C+reply&direction=DESC"
     search_url = "https://quasarzone.com/bbs/qb_saleinfo?_method=post&type=&page=1&_token=Q7kaCjkdlWWa9Q6lPxPZopa4URVoxDkq4VKwgCFM&category=&popularity=&kind=subject&keyword="+formatted_target+"&sort=num%2C+reply&direction=DESC"
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
                 'referer': 'referer: https://quasarzone.com/bbs/qb_saleinfo?_method=post&type=&page=1&_token=Q7kaCjkdlWWa9Q6lPxPZopa4URVoxDkq4VKwgCFM&category=&popularity=&kind=subject&keyword=3060+ti&sort=num%2C+reply&direction=DESC'}
 
+    try:
+        page = requests.get(search_url, headers=headers)
+        html = bs(page.text, "html.parser")
+        items = html.select('div.market-info-list-cont')
 
-    page = requests.get(search_url, headers=headers)
-    html = bs(page.text, "html.parser")
-
-    items = html.select('div.market-info-list-cont')
-
-    print("Search [{}] {}".format(type, formatted_target))
-
-    items_info = []
-    for item in items:
-        name = item.select_one('span.ellipsis-with-reply-cnt').text
-        status = item.select_one('p.tit > span').text
-        link = item.select_one('p.tit > a')['href']
-        release_date = reformat_date(item)
-        cost = int(''.join(filter(str.isdigit, item.select_one('span.text-orange').text)))
+        print("Search [{}] {}".format(type, formatted_target))
+        items_info = []
+        
+        for item in items:
+            name = item.select_one('span.ellipsis-with-reply-cnt').text
+            status = item.select_one('p.tit > span').text
+            link = item.select_one('p.tit > a')['href']
+            release_date = reformat_date(item)
+            cost = int(''.join(filter(str.isdigit, item.select_one('span.text-orange').text)))
 
         if status != '종료' \
-            and '품절' not in name \
-            and release_date > date.today() - timedelta(days=7) \
-            and cost < target_maximum_cost:
-            
-            item_info = {
-                    'date':release_date,
-                    'name':name,
-                    'status':status,
-                    'cost':cost,
-                    'link':link,
-                }
-            items_info.append(item_info)
-            
+                and '품절' not in name \
+                and release_date > date.today() - timedelta(days=7) \
+                and cost < target_maximum_cost:
 
-    print(items_info)
+                item_info = {
+                        'date':release_date,
+                        'name':name,
+                        'status':status,
+                        'cost':cost,
+                        'link':"https://quasarzone.com"+link,
+                    }
+                items_info.append(item_info)
+    except Exception as e:
+        print("Error : ", e)
+        return None
+
+    return items_info
 
 
-search_target('gpu')
-search_target('cpu')
+def repeat_searching():
+    repeat = True
+    while repeat:
+        time.sleep(5)
+        print("We will search {}".format(list(target.keys())))
+        for type in target.keys():
+            print("Search [{}] {}".format(type, target[type]['name']), end=" ")
+            result = search_target(type)
+            if result: 
+                try:
+                    send_message(result)
+                    repeat = False
+                except Exception as e:
+                    print("Error : ", e)
+                finally:
+                    repeat = False
+
+            else:
+                print("FAIL....")
+
+
+def send_message(result):
+    id = bot.getUpdates()[0].message.chat.id
+    for item in result:
+        bot.sendMessage(id, f"{item['name']}\t:\t{item['cost']} 원 \n\t-> {item['link']}")
+
+
+# ===================================== run ===================================== #
+if __name__ == '__main__':
+    print("Program Start")
+    repeat_searching()
